d2c50d1851e3a7773cabddb5042efdc040055a1e
Jeany Hsu <g200421@gmail.com>
2021-05-09 22:52:54 +0800

    add product

---
+++ product.py
@@ -0,0 +1,14 @@
+products = []
+while True:
+	name = input('請輸入商品名稱:')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格:')
+	p = [] #小清單
+	p.append(name) #append 加到小清單
+	p.append(price)
+	products.append(p) #其實可以直接寫成 p = [name, price] #更簡潔的方式為 products.append([name, price])
+print(products)
+
+products[0][0] #把第零的商品拿出來
+
